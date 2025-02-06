a7c6077595b01a1569136909f8ff985838e13c7a
Dan Miller <dan.miller@fraculation.com>
2025-02-06 07:52:35 -0600

    pull update script from hosted site. make script do thing

--- update.js
+++ update.js
@@ -1,1 +1,5 @@
 console.log("update.js loaded");
+
+var className = document.getElementById("downloadButton").className;
+className += " extraClass";
+document.getElementById("downloadButton").className = className;
